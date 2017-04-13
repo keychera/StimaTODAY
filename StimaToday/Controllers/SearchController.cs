9b628c03eaf06325ab03e46c07ad8f101a81aaaa
keychera <kyogatama@gmail.com>
2017-04-14 00:00:26 +0700

    Add project files.

---
+++ StimaToday/Controllers/SearchController.cs
@@ -0,0 +1,26 @@
+﻿using SimpleFeedReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StimaToday.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index(string searchString, string searchAlgo)
+        {
+            var feed = new FeedReader();
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                ViewData["Message"] = searchString;
+                ViewData["Algo"] = searchAlgo;
+                var items = feed.RetrieveFeed("http://www.antaranews.com/rss/terkini");
+                ViewBag.items = items;
+            }
+            return View();
+        }
+    }
+}
