70d404ff363565d0a0d5957631ee4595069a4d69
keychera <kyogatama@gmail.com>
2017-04-15 15:48:54 +0700

    :hammer: adding html agility pack for parsing the html

--- StimaToday/Controllers/SearchController.cs
+++ StimaToday/Controllers/SearchController.cs
@@ -1,7 +1,10 @@
-﻿using SimpleFeedReader;
+﻿using HtmlAgilityPack;
+using SimpleFeedReader;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,9 +18,25 @@ namespace StimaToday.Controllers
             var feed = new FeedReader();
             if (!String.IsNullOrEmpty(searchString))
             {
-                ViewData["Message"] = searchString;
-                ViewData["Algo"] = searchAlgo;
                 var items = feed.RetrieveFeed("http://www.antaranews.com/rss/terkini");
+                //using htmlagilitypack here
+                HtmlDocument htmlDoc = new HtmlDocument();
+                using (var actualArticle = new HttpClient())
+                {
+                    var response = actualArticle.GetAsync(items.ElementAt(0).Uri).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var responseContent = response.Content;
+                        htmlDoc.LoadHtml(responseContent.ReadAsStringAsync().Result);
+                    }
+                }
+                //buat ambil artikelnya, but setiap sumber berita, idnya beda2 buat artikelnya
+                //yang ini buat antaranews
+                HtmlNode node = htmlDoc.DocumentNode.SelectNodes("//div[@id='content_news']").First();
+                ViewData["Message"] = node.InnerText;
+
+                ViewData["Keywords"] = searchString;
+                ViewData["Algo"] = searchAlgo;
                 ViewBag.items = items;
             }
             return View();
